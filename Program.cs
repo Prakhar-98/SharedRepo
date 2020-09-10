e5013b471f37e34417f5819d6628933e0afed076
Prakhar-98 <70949412+Prakhar-98@users.noreply.github.com>
2020-09-10 15:54:08 +0530

    Changed folder structure

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().GetNumbers();
+            Console.ReadKey();
+        }
+        void Add()
+        {
+            Console.WriteLine("Enter 2 numbers");
+            int a, b;
+            if(int.TryParse(Console.ReadLine(),out a) && int.TryParse(Console.ReadLine(),out b))
+            {
+                if (a == 0 || b == 0)
+                    Add();
+                Console.WriteLine(a + b);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+        void Subtract()
+        {
+            Console.WriteLine("Enter 2 numbers");
+            int a, b;
+            if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+            {
+                if (a == 0 || b == 0)
+                    Subtract();
+                Console.WriteLine(a - b);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+        void GetNumbers()
+        {
+            int iNum1 = 0, iNum2 = 0;
+            iNum1 = int.MaxValue;
+            Console.WriteLine("Num 1" + iNum1);
+            checked
+            {
+                iNum1++;
+                Console.WriteLine("New Value" + iNum1);
+            }
+        }
+    }
+}
